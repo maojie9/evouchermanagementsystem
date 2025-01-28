5cb5c8c0b8c5127b906113eca2355bed1c941316
Mao Jie <maojie9@gmail.com>
2025-01-28 12:51:25 +0800

    First Commit for eVoucherManagementSystem

---
+++ src/main/java/com/maojie/evouchersystem/evouchermanagementsystem/EvouchermanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.maojie.evouchersystem.evouchermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvouchermanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvouchermanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maojie/evouchersystem/evouchermanagementsystem/EvouchermanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maojie.evouchersystem.evouchermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvouchermanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
